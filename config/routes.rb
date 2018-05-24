93111cfff9d206ed8b429be533bbf17abcb7b926
todx2 <todzone1@aol.com>
2018-05-24 16:25:26 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
- root 'application#hellox'
+   # root 'application#hellox'
+    root 'static_pages#home'
+
+
+
+  get  'static_pages/about'
 end
